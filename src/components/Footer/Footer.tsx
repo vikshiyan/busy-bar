aa659295e27441cb19f9a36e775bce69c3f7ae10
vikshiyan <miss.shiyan@gmail.com>
2025-01-16 18:27:22 +0200

    First step

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import {FC} from 'react';
+import './Footer.scss';
+
+export const Footer: FC = () => {
+  return (
+    <div className=''>
+      <h1 className=''>Footer</h1>
+    </div>
+  );
+};
